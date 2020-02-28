e157606faf596b0fccd6626e757c756701557734
Subramanian <subbucc658@gmail.com>
2020-02-28 08:48:10 -0500

    first commit

---
+++ resources/js/first.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+  new Vue({
+    el : "#app",
+    data : {
+      menuItems : {
+        technology1 : "NodeJS",
+        technology2 : "VueJs",
+        technology3 : "Rwd",
+        technology4 : "Docker",
+
+      }
+    }
+
+  });
+}
